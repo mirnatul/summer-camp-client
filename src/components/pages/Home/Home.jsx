0ec042723b002884b40f1def2fd4c551cefc3f21
Li Pon <mirnatul@gmail.com>
2023-10-18 15:54:34 +0600

    cut the extra data

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,15 +1,8 @@
 import React from 'react';
-import Slider from 'react-slick';
 import Banner from './Banner/Banner';
 import PopularClasses from './popularclass/PopularClasses';
 import TopInstructor from './Instructors/TopInstructor';
-import RocketScrollAnimation from '../../shared/RocketScrollAnimation';
-import PerformanceShowCase from '../../shared/PerformanceShowcase'
-import AnotherExtraSection from './AnotherExtraSection';
-import SampleClass from './SampleClass';
 import PageCompo from '../../shared/PageCompo';
-import OurActivity from './OurActivity';
-import RegistrationPromotion from './RegistrationPromotion';
 import Testimonials from './Testimonials';
 import WhatYouWillGet from './WhatYouWillGet';
 import Contact from './Contact/Contact';
@@ -33,8 +26,6 @@ const Home = () => {
                 <PerformanceOfOurStudents></PerformanceOfOurStudents>
                 <OurClient></OurClient>
                 <Testimonials></Testimonials>
-                {/* <OurActivity></OurActivity> */}
-                {/* <RegistrationPromotion></RegistrationPromotion> */}
                 <Plans></Plans>
                 <Contact></Contact>
                 <Faq></Faq>
